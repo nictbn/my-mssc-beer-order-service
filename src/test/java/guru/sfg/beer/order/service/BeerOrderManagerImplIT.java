db9e61206a7ebecac543425f1d1b0d46b01aa6fe
Nicu Taban <nictbndeveloper@gmail.com>
2022-05-23 01:06:30 +0300

    Adds Allocation Integration Test; WIP for #25

---
+++ src/test/java/guru/sfg/beer/order/service/BeerOrderManagerImplIT.java
@@ -0,0 +1,68 @@
+package guru.sfg.beer.order.service;
+
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.domain.BeerOrderLine;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.domain.Customer;
+import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import guru.sfg.beer.order.service.repositories.CustomerRepository;
+import guru.sfg.beer.order.service.services.BeerOrderManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class BeerOrderManagerImplIT {
+
+    @Autowired
+    BeerOrderManager beerOrderManager;
+
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    Customer testCustomer;
+
+    UUID beerId = UUID.randomUUID();
+
+    @BeforeEach
+    void setUp() {
+        testCustomer = customerRepository.save(Customer.builder()
+                .customerName("Test Customer")
+                .build());
+    }
+
+    @Test
+    void testNewToAllocated() {
+        BeerOrder beerOrder = createBeerOrder();
+
+        BeerOrder savedBeerOrder = beerOrderManager.newBeerOrder(beerOrder);
+        assertNotNull(savedBeerOrder);
+        assertEquals(BeerOrderStatusEnum.ALLOCATED, savedBeerOrder.getOrderStatus());
+    }
+
+    public BeerOrder createBeerOrder() {
+        BeerOrder beerOrder = BeerOrder.builder()
+                .customer(testCustomer)
+                .build();
+
+        Set<BeerOrderLine> lines = new HashSet<>();
+        lines.add(BeerOrderLine.builder()
+                .beerId(beerId)
+                .orderQuantity(1)
+                .beerOrder(beerOrder)
+                .build());
+        beerOrder.setBeerOrderLines(lines);
+        return beerOrder;
+    }
+}
